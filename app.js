bceb62b9d9a05173e6a7631a5b8f3ebaa6092ddc
utsavdas10 <97392403+utsavdas10@users.noreply.github.com>
2023-05-05 11:35:22 +0530

    removed redundant package mongodb

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const lodash = require('lodash')
-const mongodb = require('mongodb')
 
 const app = express();
 
