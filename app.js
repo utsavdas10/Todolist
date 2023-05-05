42238ec4041019c960471b9f1bca53d3afc93599
utsavdas10 <97392403+utsavdas10@users.noreply.github.com>
2023-05-05 20:00:04 +0530

    ||

--- app.js
+++ app.js
@@ -175,8 +175,8 @@ app.get("/:customListName", function(req, res){
 
 
 
+const port = process.env.PORT || 5000
 
-
-app.listen(3000, function() {
-  console.log("Server started on port 3000...");
+app.listen(port, function() {
+  console.log(`Server started on port ${port}...`);
 });
